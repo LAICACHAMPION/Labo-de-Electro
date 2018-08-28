5a856f818024bebac7edb63a9d92924d9b93d612
rocioparra <rparra@itba.edu.ar>
2018-08-28 12:48:01 -0300

    correcciones ej2, caratula, indice

--- Codigo Matlab/labo_tp1_ej4.m
+++ Codigo Matlab/labo_tp1_ej4.m
@@ -1,8 +1,4 @@
-c_m=csvread('labo_tp1_ej4_sheet1.csv');
+c_m=csvread('labo_tp1_ej4_sheet2.csv');
 magMedida=20*log10(c_m(:,3)./c_m(:,2));
 semilogx(c_m(:,1).*1000,magMedida,'-o');
 formataxes('', 'Frecuencia (Hz)', 'Mangnitud (dB)')% legends
-                
-figure;                
-semilogx(c_m(:,1).*1000,c_m(:,4), '-o');
-formataxes('', 'Frecuencia (Hz)', 'Fase ($^\circ$)')% legends
